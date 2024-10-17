d8638a334b91e1b1c411c0f770a6c5fbfbc2bd27
sillyducklin <34515460+sillyducklin@users.noreply.github.com>
2024-10-16 22:40:47 -0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
 ::.\build\mingw64\bin\gcc.exe main.c -o main.exe -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows
 :: -mwindows
 :: gcc main.c -o main -O1 -Wall -std=c99 -Wno-missing-braces -L ./lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
-.\build\mingw64\bin\gcc.exe main.c -o main.exe -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows
+SET GCC=".\build\mingw64\bin\gcc.exe"
+%GCC% main.c -o main.exe .\build\lib\libraylib.a -L .\build\lib -I .\build\include\ -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows
 PAUSE
